64a706168b21646243d07f74e078bf037853a022
rishabhkathait39 <rishabhkathait39@gmail.com>
2024-10-01 10:12:39 +0530

    new-updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import {FaStar} from 'react-icons/fa'
+
 import './App.css'
+import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({noOfStars = 5 }) {
+  
+   
+     const [rating, setRating] = useState(0);
+     const [hover, setHover] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     function handelClick(getCrrentIndex) {
+      setRating(getCrrentIndex)
+     }
+
+     function handeMouseEnter(getCrrentIndex) {
+      setHover(getCrrentIndex)
+     }
+
+     function handeMouseLeave()  { 
+      setHover(rating)
+     }
+
+    
+  
+  return <div className='star-rating'>
+    
+   {
+      [...Array(noOfStars)].map((_,index) => {
+          index += 1
+          return <FaStar
+            
+          key={index}
+          className={index <= (hover || rating) ? 'active' : 'inactive'}
+          onClick={()=> handelClick(index)}
+          onMouseMove={()=> handeMouseEnter(index)}
+          onMouseLeave={()=> handeMouseLeave()}
+          size={40}
+
+          />
+      })
+      
+      
+   }
+
+
+  </div>
+   
+  
 }
 
 export default App
